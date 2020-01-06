8bb442d5f2c353f19d45cddf2c72c41b76081353
quanwei.luo <quanwei.luo@qiaofang.local>
2020-01-06 18:26:35 +0800

    添加测试类，测试java值传递

---
+++ src/test/java/com/lqw/MyTest.java
@@ -0,0 +1,29 @@
+package com.lqw;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyTest {
+    Logger logger= LoggerFactory.getLogger(MyTest.class);
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void name() {
+        StringBuilder x = new StringBuilder("name");
+
+        change(x);
+
+        logger.info(x.toString());
+
+    }
+
+    public void  change(StringBuilder x){
+        x.append("LLL")
+                ;
+        x=new StringBuilder("change");
+    }
+}
