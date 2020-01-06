7f96674cbbf7507932c699ba80f17db2db08d827
quanwei.luo <quanwei.luo@qiaofang.local>
2020-01-06 18:04:01 +0800

    链表操作-单链表反转

--- src/main/java/com/lqw/linked/LinkedReverse.java
+++ src/main/java/com/lqw/linked/LinkedReverse.java
@@ -1,9 +1,14 @@
 package com.lqw.linked;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 单链表反转
  */
 public class LinkedReverse {
+
+    public static final  Logger LOGGER = LoggerFactory.getLogger(LinkedReverse.class);
 
     public static void main(String[] args) {
         OwnWayLinked<String> ownWayLinked =new OwnWayLinked<>();
@@ -22,7 +27,7 @@ public class LinkedReverse {
         foreach(ownWayLinked.getHead());
 
         OwnWayLinked.Node<String> currentNode = ownWayLinked.getHead();
-        System.out.println("------------------------------------------------------------------------");
+        LOGGER.info("------------------------------------------------------------------------");
         //下面进行反转
         //记录当前节点为下一个几点
         OwnWayLinked.Node<String> prev=currentNode;
@@ -44,7 +49,7 @@ public class LinkedReverse {
     public static void foreach(OwnWayLinked.Node<String> currentNode ) {
         //下面进行遍历
         while (currentNode!=null){
-            System.out.println(currentNode.getData());
+            LOGGER.info(currentNode.getData());
             currentNode = currentNode.getNext();
         }
     }
