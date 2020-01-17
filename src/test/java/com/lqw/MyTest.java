d7df14175e5c1e84e70ecf471cf969a48ed87944
quanwei.luo <quanwei.luo@qiaofang.local>
2020-01-17 11:34:50 +0800

    测试功能提交

--- src/test/java/com/lqw/MyTest.java
+++ src/test/java/com/lqw/MyTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+
 public class MyTest {
     private Logger logger= LoggerFactory.getLogger(MyTest.class);
     public static void main(String[] args) {
@@ -25,5 +27,16 @@ public class MyTest {
         x.append("LLL")
                 ;
         x=new StringBuilder("change");
+    }
+
+    @Test
+    public void ssss() {
+        String num= "17145004032";
+        BigDecimal bigDecimal = new BigDecimal(num);
+        BigDecimal big = BigDecimal.valueOf(1024);
+        for (int i = 0; i < 3; i++) {
+            bigDecimal = bigDecimal.divide(big);
+        }
+        System.out.printf(bigDecimal.doubleValue()+"");
     }
 }
