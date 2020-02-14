fe267f3c3f374425b11e7e8c04ab3d27fe3d7709
quanwei.luo <quanwei.luo@qiaofang.local>
2020-02-14 10:05:43 +0800

    桶排序定义，未实现

---
+++ src/main/java/com/lqw/sort/n/BucketSort.java
@@ -0,0 +1,16 @@
+package com.lqw.sort.n;
+
+import com.lqw.sort.Sortable;
+
+/**
+ * 桶排序：
+ * 针对大量数据进行排序的算法。
+ * 将数据
+ */
+public class BucketSort implements Sortable {
+
+    @Override
+    public void sort(int[] arr) {
+
+    }
+}
